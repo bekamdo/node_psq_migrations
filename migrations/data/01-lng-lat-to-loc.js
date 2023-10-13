728e52379ae0248bf6126f09645ac0908590fff5
bekamdo <bekamdo@gmail.com>
2023-10-13 11:56:19 +0300

    node-psql-migrations

---
+++ migrations/data/01-lng-lat-to-loc.js
@@ -0,0 +1,24 @@
+const pg = require("pg");
+
+const pool = new pg.Pool({
+  host: "localhost",
+  port: 5432,
+  database: "socialnetwork",
+  user: "postgres",
+  password: "codename",
+});
+
+pool
+  .query(
+    `
+UPDATE posts
+SET loc= POINT(lng,lat)
+WHERE loc IS NULL;
+
+`
+  )
+  .then(() => {
+    console.log("Update complete");
+    pool.end();
+  })
+  .catch((err) => console.error(err.message));
